964949138a6503082e8cb47919208fd1fa0a50e0
“Sakyy” <sakydgny@gmail.com>
2026-03-07 10:54:15 +0700

    instalasi uploadthing

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import "dotenv/config.js";
 import cors from "cors";
 import Routes from "./routes/index.js";
 import db from "./config/db.js";
+import { createRouteHandler } from "uploadthing/express";
+import { uploadRouter } from "./upload/uploadthing.js";
 
 let port = 2000;
 let app = express();
@@ -9,6 +12,8 @@ let app = express();
 app.use(cors());
 app.use(express.json());
 app.use(Routes);
+
+app.use("/api/upload-image", createRouteHandler({ router: uploadRouter }));
 async function getTable(){
     await db.sync()
     console.log('table success added')
